99a65b1f4572a91e8e648bc9dda5a9f4d0c91b29
judixel <judixel2015@ya.ru>
2024-11-12 00:15:02 +0400

    update login page

--- src/interfaces/login/pages/LoginPage.vue
+++ src/interfaces/login/pages/LoginPage.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <h2>Login</h2>
+  <div
+    class="mx-auto mt-4 w-[90%] max-w-[1280px] flex flex-col justify-center px-6 py-8 duration-500"
+  >
+    <div class="flex w-full flex-1 flex-col max-w-screen-xl mx-auto">
+      <h2>Login</h2>
+    </div>
   </div>
 </template>
 
